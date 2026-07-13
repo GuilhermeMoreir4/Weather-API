43d431b5c73656b7fe66e68dd2a4b07d15434260
Guilherme Jacinto de Araujo Moreira <gulhermejacinto1@hotmail.com>
2026-07-13 13:53:05 -0300

    feat: starting project

---
+++ src/main/java/com/example/whaterapi/WhaterApiApplication.java
@@ -0,0 +1,13 @@
+package com.example.whaterapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WhaterApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WhaterApiApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/whaterapi/WhaterApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.whaterapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WhaterApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
