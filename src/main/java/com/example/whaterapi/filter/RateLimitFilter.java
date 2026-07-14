d57ec0a8255e8b73286b68e49cc2d420f3686024
Guilherme Jacinto de Araujo Moreira <gulhermejacinto1@hotmail.com>
2026-07-14 14:47:03 -0300

    feat: rating limit.

---
+++ src/main/java/com/example/whaterapi/filter/RateLimitFilter.java
@@ -0,0 +1,50 @@
+package com.example.whaterapi.filter;
+
+import io.github.bucket4j.Bandwidth;
+import io.github.bucket4j.Bucket;
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import org.springframework.stereotype.Component;
+import org.springframework.web.filter.OncePerRequestFilter;
+
+import java.io.IOException;
+import java.time.Duration;
+import java.util.Map;
+import java.util.concurrent.ConcurrentHashMap;
+
+@Component
+public class RateLimitFilter extends OncePerRequestFilter {
+
+    private final Map<String, Bucket> buckets = new ConcurrentHashMap<>();
+
+    private Bucket resolveBucket(String ip){
+
+        return buckets.computeIfAbsent(ip,key -> {
+            Bandwidth limit = Bandwidth.builder()
+                    .capacity(2)
+                    .refillGreedy(2, Duration.ofMinutes(1))
+                    .build();
+
+            return Bucket.builder()
+                    .addLimit(limit)
+                    .build();
+        });
+    }
+
+    @Override
+    protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException {
+        String ip = request.getRemoteAddr();
+
+        Bucket bucket = resolveBucket(ip);
+
+        if(bucket.tryConsume(1)){
+            filterChain.doFilter(request,response);
+        }else{
+            response.setStatus(429);
+            response.getWriter()
+                    .write("Too many requests");
+        }
+    }
+}
